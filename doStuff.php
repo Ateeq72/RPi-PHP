ccae9b7265b497206f97ea7deaddcb9a3c5ecbbd
Ateeq Ahmed <ateeq@halsimplify.com>
2016-08-10 17:05:34 +0530

    Fix unused Logic

--- doStuff.php
+++ doStuff.php
@@ -15,13 +15,7 @@
  class doStuff
  {
 
-     public $upUsed ;
-     public $downUsed ;
-     public $leftUsed ;
-     public $rightUsed ;
-
-
-     public function __construct($action)
+   public function __construct($action)
      {
 
          if (isset($action)) {
@@ -65,10 +59,6 @@
          shell_exec('/usr/local/bin/gpio write 18 0');
          shell_exec('/usr/local/bin/gpio write 22 0');
          shell_exec('/usr/local/bin/gpio write 24 0');
-         $this->upUsed = 0;
-         $this->downUsed = 0;
-         $this->leftUsed = 0;
-         $this->rightUsed = 0;
      }
 
      public function end()
@@ -77,49 +67,29 @@
          shell_exec('/usr/local/bin/gpio write 18 0');
          shell_exec('/usr/local/bin/gpio write 22 0');
          shell_exec('/usr/local/bin/gpio write 24 0');
-         $this->upUsed = 0;
-         $this->downUsed = 0;
-         $this->leftUsed = 0;
-         $this->rightUsed = 0;
      }
 
      public function up()
      {
-         if ($this->upUsed == 0) {
              shell_exec('/usr/local/bin/gpio write 16 1');
              shell_exec('/usr/local/bin/gpio write 18 0');
-             $this->upUsed = 1;
-             $this->downUsed = 0;
-         }
      }
 
      public function down()
      {
-         if ($this->downUsed == 0) {
              shell_exec('/usr/local/bin/gpio write 16 0');
              shell_exec('/usr/local/bin/gpio write 18 1');
-             $this->downUsed = 1;
-             $this->upUsed = 0;
-         }
      }
 
      public function left()
      {
-         if ($this->leftUsed == 0) {
              shell_exec('/usr/local/bin/gpio write 22 1');
              shell_exec('/usr/local/bin/gpio write 24 0');
-             $this->leftUsed = 1;
-             $this->rightUsed = 0;
-         }
      }
 
      public function right()
      {
-         if ($this->rightUsed == 0) {
              shell_exec('/usr/local/bin/gpio write 22 0');
              shell_exec('/usr/local/bin/gpio write 24 1');
-             $this->rightUsed = 1;
-             $this->leftUsed = 0;
-         }
      }
  }
