5f309b01d960eb2168593931963afed932930de9
Ateeq Ahmed <ateeq@halsimplify.com>
2017-08-09 13:08:22 +0530

    Add More stuff

---
+++ Ismail/index.php
@@ -0,0 +1,15 @@
+<?php
+echo "Hello World";
+?>
+<table border=1>
+<tr>
+<td>First!</td>
+<td>First!</td>
+</tr>
+<tr>
+<td>First!</td>
+<td>First!</td>
+</tr>
+</table>
+<input type="text" name="test" value="Example"/>
+<input type="button" name="test" value="Example"/>
