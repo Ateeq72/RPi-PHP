92895dcdb125ada3c6c9ad75f8b117976c25628b
Ateeq Ahmed <ahmedateeq64@gmail.com>
2018-03-03 23:54:28 +0530

    Make the app work

--- platforms/android/app/build/intermediates/assets/debug/www/js/app.js
+++ platforms/android/app/build/intermediates/assets/debug/www/js/app.js
@@ -1,11 +1,55 @@
 /**
  * Created by ateeq-ahmed on 11/2/18.
  */
-document.addEventListener("deviceready", onDeviceReady, false);
 
-function onDeviceReady() {
+var storage = window.localStorage;
 
-    alert('Hello!');
+var getIpAndPort = function(){
+
+    var ip = this.storage.getItem('ip');
+    var port = this.storage.getItem('port');
+
+    return {'ip' : ip, 'port' : port};
+
+};
+
+var setIpAndPort = function(ip,port){
+
+    this.storage.setItem('ip', ip);
+    this.storage.setItem('port', port);
 
 
+};
+
+function sendTo(data, addr, port) {
+
+    if (addr == null || port == null || addr == undefined || port == undefined || addr == '' || port == ''){
+        alert('IP or Port not set!');
+    }
+
+    data = stringToArrayBuffer(data);
+
+    chrome.sockets.udp.create(function(createInfo) {
+        chrome.sockets.udp.bind(createInfo.socketId, '0.0.0.0', 0, function(result) {
+            chrome.sockets.udp.send(createInfo.socketId, data, addr, port, function(result) {
+                if (result < 0) {
+                    console.log('send fail: ' + result);
+                    chrome.sockets.udp.close(createInfo.socketId);
+                } else {
+                    console.log('sendTo: success ' + port);
+                    chrome.sockets.udp.close(createInfo.socketId);
+                }
+            });
+        });
+    });
+}
+
+function stringToArrayBuffer(string) {
+    // UTF-16LE
+    var buf = new ArrayBuffer(string.length * 2);
+    var bufView = new Uint16Array(buf);
+    for (var i = 0, strLen = string.length; i < strLen; i++) {
+        bufView[i] = string.charCodeAt(i);
+    }
+    return buf;
 }
--- platforms/android/app/src/main/assets/www/js/index.js
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -13,31 +13,8 @@ var app = {
     // Bind any cordova events here. Common events are:
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
-
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-
-        sendTo(new ArrayBuffer('hello'),'192.168.0.77',8777);
 
     }
 };
 
 app.initialize();
-
-function sendTo(data, addr, port) {
-    chrome.sockets.udp.create(function(createInfo) {
-        chrome.sockets.udp.bind(createInfo.socketId, '0.0.0.0', 0, function(result) {
-            chrome.sockets.udp.send(createInfo.socketId, data, addr, port, function(result) {
-                if (result < 0) {
-                    console.log('send fail: ' + result);
-                    chrome.sockets.udp.close(createInfo.socketId);
-                } else {
-                    console.log('sendTo: success ' + port);
-                    chrome.sockets.udp.close(createInfo.socketId);
-                }
-            });
-        });
-    });
-}
